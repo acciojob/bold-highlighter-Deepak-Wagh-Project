6f607651290d47c027c44a8843906b77abf1e14c
Deepak-Wagh-Project <133074729+Deepak-Wagh-Project@users.noreply.github.com>
2023-12-14 14:40:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+for(let i=0;i<text.length;i++){
+    text[i].style.backgroundColor='green'
+}
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+    for(let i=0;i<text.length;i++){
+        text[i].style.backgroundColor='transparent'
+    }
     
 }
+
+const text=document.getElementsByTagName('strong')
